6daf412e5026d807aadc4b397ae510a10bb3e6df
Chhan Rathana <chhrathana@gmail.com>
2023-01-10 00:21:20 +0700

    JWT token

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->group(['prefix' => 'auth', 'as' => 'auth'], function () use ($router) {
+        $router->post('login',    ['as' => 'login',     'uses' => 'AuthController@login']);
+        $router->post('logout',   ['as' => 'logout',   'uses' => 'AuthController@logout']);
+        $router->post('refresh',   ['as' => 'refresh',   'uses' => 'AuthController@refresh']);
+        $router->post('me',   ['as' => 'me',   'uses' => 'AuthController@me']);
+    });
+});
